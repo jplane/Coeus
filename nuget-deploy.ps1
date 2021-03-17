b922a3599a93a7b231e7dd43f30c6bd5c478b157
Josh Lane <jolan@microsoft.com>
2021-03-17 13:37:17 -0400

    updated to v0.2

--- nuget-deploy.ps1
+++ nuget-deploy.ps1
@@ -1,4 +1,4 @@
 
 $key = ''
 
-dotnet nuget push .\src\bin\Release\Coeus.0.1.0.nupkg --api-key $key --source https://api.nuget.org/v3/index.json
+dotnet nuget push .\src\bin\Release\Coeus.0.2.0.nupkg --api-key $key --source https://api.nuget.org/v3/index.json
